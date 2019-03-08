900d3003259e20885ddbd6a1e71b9c49cae91ccd
James Hughes <jim.d.hughes@gmail.com>
2019-03-08 09:27:02 -0700

    Update cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -33,7 +33,7 @@ var listCmd = &cobra.Command{
 var apiCmd = &cobra.Command{
 	Use:   "api",
 	Short: "Start API",
-	Run:   StartApi,
+	Run:   ApiCmd,
 }
 
 func Execute() {
@@ -75,7 +75,7 @@ func ListCmd(cmd *cobra.Command, args []string) {
 	fmt.Println("---\nDone")
 }
 
-func StartApi(cmd *cobra.Command, args []string) {
+func ApiCmd(cmd *cobra.Command, args []string) {
 	r := api.NewRouter(&store)
 	log.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", r)
